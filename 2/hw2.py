7ad689e559df16978b7342f20bf6f71585e7af5e
Borys Pochekailo <Po4ek634289@gmail.com>
2025-08-31 18:00:13 +0200

    hw 9-4 , 9-5 (some changes)

--- 2/hw2.py
+++ 2/hw2.py
@@ -19,26 +19,6 @@ properly, and then call reset_login_attempts(). Print login_attempts again to
 make sure it was reset to 0."""
 
 
-"""TRY IT YOURSELF
-9-1. Restaurant: Make a class called Restaurant. The __init__() method for
-Restaurant should store two attributes: a restaurant_name and a cuisine_type.
-Make a method called describe_restaurant() that prints these two pieces of
-information, and a method called open_restaurant() that prints a message indi-
-cating that the restaurant is open.
-Make an instance called restaurant from your class. Print the two attri-
-butes individually, and then call both methods.
-9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three
-different instances from the class, and call describe_restaurant() for each
-instance.
-9-3. Users: Make a class called User. Create two attributes called first_name
-and last_name, and then create several other attributes that are typically stored
-in a user profile. Make a method called describe_user() that prints a summary
-of the user’s information. Make another method called greet_user() that prints
-a personalized greeting to the user.
-Create several instances representing different users, and call both meth-
-ods for each user."""
-
-
 # 9-4
 class Restaurant:
 
@@ -46,7 +26,6 @@ class Restaurant:
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
         self.number_served = 0
-
 
     def describe_restaurant(self):
         print(f"This is {self.restaurant_name} restaurant. We have {self.cuisine_type} cuisine.")
@@ -81,12 +60,10 @@ class User():
         self.sex = sex
         self.login_attempts = 0
 
-
     def describe_user(self):
         print(f"User name is {self.first_name} {self.last_name}.")
         print(f"Age: {self.age} years old.")
         print(f"Sex: {self.sex}.")
-
 
     def greet_user(self):
         print(f"Hello {self.first_name} {self.last_name}.")
@@ -104,11 +81,9 @@ user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
-
 print(f"User {user1.first_name} {user1.last_name} has tried to login {user1.login_attempts} times!")
 
 user1.reset_login_attempts()
-
 print(f"User {user1.first_name} {user1.last_name} has tried to login {user1.login_attempts} times!")
 
 
