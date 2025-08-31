a16e742ad3ff9d990a71cf1ab4b098714102631e
Borys Pochekailo <Po4ek634289@gmail.com>
2025-08-31 16:32:02 +0200

    hw 9-2 , 9-3

--- hw1.py
+++ hw1.py
@@ -18,7 +18,7 @@ Create several instances representing different users, and call both meth-
 ods for each user."""
 
 
-
+# 9-1
 class Restaurant:
 
     def __init__(self, restaurant_name, cuisine_type):
@@ -35,5 +35,48 @@ class Restaurant:
 
 mamba = Restaurant("Mamba", "Ukrainian")
 
-mamba.open_restaurant()
-mamba.describe_restaurant()
+# mamba.open_restaurant()
+# mamba.describe_restaurant()
+
+
+
+#9-2
+nika = Restaurant("Nika", "Italian")
+protokol = Restaurant("Protokol", "Greek")
+
+# mamba.describe_restaurant()
+# nika.describe_restaurant()
+# protokol.describe_restaurant()
+
+
+class User():
+    def __init__(self, first_name, last_name, age, sex):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.sex = sex
+
+
+    def describe_user(self):
+        print(f"User name is {self.first_name} {self.last_name}.")
+        print(f"Age: {self.age} years old.")
+        print(f"Sex: {self.sex}.")
+
+
+    def greet_user(self):
+        print(f"Hello {self.first_name} {self.last_name}.")
+
+user1 = User('Ivan', 'Korba', 19, 'M')
+user2 = User('Andrii', 'Manko', 25, 'M')
+user3 = User('Anna', 'Lesiv', 20, 'W')
+
+
+user1.describe_user()
+user1.greet_user()
+print('\n')
+user2.describe_user()
+user2.greet_user()
+print('\n')
+user3.describe_user()
+user3.greet_user()
+print('\n')
